5d407f04b10600d0bbf758b39b0de4045d78caed
Sulaimon Taofeek Ayomide <sulaimontaofeekgithub100@gmail.com>
2024-09-18 14:47:55 +0100

    update on experience page

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -54,7 +54,7 @@ const Experience = () => {
                             <BsPatchCheckFill className='experience__details-icon' />
                             <div>
                                 <h4>Vuejs</h4>
-                                <small className='text-light'>Intermediate</small>
+                                <small className='text-light'>Experience</small>
                             </div>
                         </article>
                         <article className='experience__details'>
